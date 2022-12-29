a79118f1aaf964b5fb7981b05390bf8bf4695986
zayer97 <121635997+zayer97@users.noreply.github.com>
2022-12-29 20:41:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a n1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a n2");
+            int n2 = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int x = n1; x <= n2; x++)
+            {
+                sum = 0;
+
+                for (int i = 1; i <= x / 2; i++)
+                {
+                    if (x % i == 0)
+                        sum += i;
+                }
+                if (sum == x)
+                    Console.WriteLine(x);
+            }
+        }
+    }
+}
